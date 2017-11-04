42fa8d535bb777dc84e974cded950091baaf0885
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-11-04 11:45:01 -0400

    Prompt written.

--- CPP-PRG-10-12-Password-Verifier/main.cpp
+++ CPP-PRG-10-12-Password-Verifier/main.cpp
@@ -5,11 +5,24 @@
 //  Created by Keith Smith on 11/4/17.
 //  Copyright © 2017 Keith Smith. All rights reserved.
 //
+//  Imagine you are developing a software package that requires users to enter their own
+//  passwords. Your software requires that the users' passwords meet the following criteria:
+//
+//      * The password should be at least six characters long.
+//      * The password should contain at least one uppercase and at least one lowercase letter.
+//      * The password should have at least one digit.
+//
+//  Write a program that asks for a password then verifies that it meets the stated criteria.
+//  If it doesn't, the program should display a message telling the user why.
+//
 
 #include <iostream>
+#include <cstring>
+#include <cctype>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main()
+{
+    
+    
     return 0;
 }
